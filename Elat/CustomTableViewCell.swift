30b94843c46b0e928f7815d371367551b22528e6
developer1982 <r.wallin1982@outlook.com>
2016-01-04 14:09:55 +0100

    no message

---
+++ Elat/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  Elat
+//
+//  Created by admin on 1/4/16.
+//  Copyright © 2016 admin. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
