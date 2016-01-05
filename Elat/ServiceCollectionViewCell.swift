4b7a73e2ecb5ce3c91d8ad4f19185bec2c942626
developer1982 <r.wallin1982@outlook.com>
2016-01-05 08:19:43 +0100

    no message

---
+++ Elat/ServiceCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ServiceCollectionViewCell.swift
+//  Elat
+//
+//  Created by admin on 1/4/16.
+//  Copyright © 2016 admin. All rights reserved.
+//
+
+import UIKit
+
+class ServiceCollectionViewCell: UICollectionViewCell {
+    
+}
