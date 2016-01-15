69fb39346e9b8782a2bc208e2c31894cd2d33103
developer1982 <r.wallin1982@outlook.com>
2016-01-15 08:12:47 +0100

    beta version

---
+++ Elat/ListDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ListDetailViewController.swift
+//  Elat
+//
+//  Created by admin on 1/14/16.
+//  Copyright © 2016 admin. All rights reserved.
+//
+
+import Cocoa
+
+class ListDetailViewController: UIViewController {
+
+}
---
+++ Elat/ListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListTableViewCell.swift
+//  Elat
+//
+//  Created by admin on 1/13/16.
+//  Copyright © 2016 admin. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
